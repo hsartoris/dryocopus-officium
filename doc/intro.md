846ef5e1e7394939f414f1224803d305077ce0c1
Hayden Sartoris <hsartoris@gmail.com>
2020-07-12 20:05:01 -0400

    basic project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dryocopus-officium
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dryocopus_officium/core_test.clj
@@ -0,0 +1,7 @@
+(ns dryocopus-officium.core-test
+  (:require [clojure.test :refer :all]
+            [dryocopus-officium.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
