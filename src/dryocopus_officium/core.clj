ae3e08a9515bc9db3b29fbe52f29119aa28d349b
Hayden Sartoris <hsartoris@gmail.com>
2020-09-05 07:56:07 -0400

    load links

--- src/dryocopus_officium/core.clj
+++ src/dryocopus_officium/core.clj
@@ -1,5 +1,14 @@
 (ns dryocopus-officium.core
+  (:require [lambdaisland.uri :refer [uri]]
+            [clojure.java.io :as io])
   (:gen-class))
+
+
+(def links (->> "links"
+                io/resource
+                slurp
+                clojure.string/split-lines
+                (mapv uri)))
 
 (defn -main
   "I don't do a whole lot ... yet."
