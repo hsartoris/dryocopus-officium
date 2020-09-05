571f053c90d94457f93a830afe43d3e7d2a7d1c4
Hayden Sartoris <hsartoris@gmail.com>
2020-09-05 07:56:33 -0400

    basic scraping test

---
+++ src/dryocopus_officium/scrape.clj
@@ -0,0 +1,24 @@
+(ns dryocopus-officium.scrape
+  (:require [lambdaisland.uri :refer [uri]]
+            [clojure.java.io :as io]
+            [hickory.core :as h]
+            [hickory.select :as s]
+            [clj-http.client :as http]))
+
+(defn get-tree [uri]
+  (-> uri str http/get :body h/parse h/as-hickory))
+
+(defmulti scrape (fn [uri] (:host uri)))
+
+(defmethod scrape "www.ripleyconservancy.org"
+  [uri]
+  (s/select (s/child (s/class :wsite-section-elements)
+                     (s/class :paragraph))
+            (get-tree uri)))
+
+(defmethod scrape "www.dec.ny.gov"
+  [uri]
+  ;(s/select (s/title "Positions Open to the General Public")
+  ; (s/select (s/attr   ; TODO
+  (s/select (s/id :contentContainer)
+            (get-tree uri)))
