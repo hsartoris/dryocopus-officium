0058239bfda943e2a63ac5b816d95031a7321d4f
Hayden Sartoris <hsartoris@gmail.com>
2020-09-05 09:59:31 -0400

    some stuff

--- src/dry_off/notify.clj
+++ src/dry_off/notify.clj
@@ -1,8 +1,18 @@
 (ns dry-off.notify
-  (:require [postal.core :refer [send-message]]))
+  (:require [postal.core :refer [send-message]]
+            [postal.support :refer [message-id]]))
+
+(def host
+  {:host "localhost"
+   :user "user"
+   :pass "pass"
+   :port 587
+   :tls true})
 
 (def ex-msg
   {:from "jobs@sa.rtor.is"
-   :to "hsartoris@localhost"
+   :to "hsartoris@gmail.com"
+   ;:to "hsartoris@localhost"
    :subject "Test"
-   :body "Hello world"})
+   :body "Hello world"
+   :message-id #(message-id "sa.rtor.is")})
