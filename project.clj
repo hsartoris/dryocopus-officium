a29dcf19567b6caf132f09df223e2fa0f090aaf0
Hayden Sartoris <hsartoris@gmail.com>
2020-09-05 07:55:38 -0400

    parsing deps

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [lambdaisland/uri "1.4.54"]
+                 [hickory "0.7.1"]
+                 [clj-http "3.10.1"]]
   :main ^:skip-aot dryocopus-officium.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
